c95f9956311b0762b897800ccf46aecedc0736a6
xwmtp <xwillmtp@gmail.com>
2022-01-23 20:37:43 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
+// https://adventofcode.com/2015/day/1
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        val upCount = input.count { it == '(' }
+        val downCount = input.count { it == ')' }
+        return upCount - downCount
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        var floor = 0
+
+        return input.toList()
+            .asSequence()
+            .onEach {
+                if (it == '(') floor++ else floor--
+            }.indexOfFirst { floor == -1 } + 1
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInputString("Input01")
     println(part1(input))
     println(part2(input))
 }
